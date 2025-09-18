c8e3264f1bf69da3d0357a58e528ff4e66031633
Shubham Chouhan <shubamc04@gamil.com>
2025-09-18 17:07:10 +0530

    add new file

--- social media/src/component/Post.jsx
+++ social media/src/component/Post.jsx
@@ -2,8 +2,15 @@ const Post = ({post }) => {
   return (
         <div className="card post-card" style={{width: "18rem;"}}>
   <div className="card-body">
-    <h5 className="card-title">{post.title}</h5>
+    <h5 className="card-title">{post.title}
+      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    {post.reaction}
+    </span>
+    </h5>
     <p className="card-text">{post.body}</p>
+    {post.tags.map((tag) =>{
+      <span className="bagde text-bg-primary hastag">{tag}</span>
+    })}
    
   </div>
 </div>
